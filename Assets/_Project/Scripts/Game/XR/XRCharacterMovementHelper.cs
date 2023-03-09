1e675bc8a998acfc227fac431cc66e1d99d9b55b
Bohdan Tsaplia <b.tsaplia@gmail.com>
2023-03-09 23:12:53 +0200

    character movement improvements

---
+++ Assets/_Project/Scripts/Game/XR/XRCharacterMovementHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Unity.XR.CoreUtils;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace Gisha.Destruction.Game.XR
+{
+    [RequireComponent(typeof(XROrigin), typeof(CharacterControllerDriver), typeof(CharacterController))]
+    public class XRCharacterMovementHelper : MonoBehaviour
+    {
+        private XROrigin _xrOrigin;
+        private CharacterController _characterController;
+        private CharacterControllerDriver _driver;
+
+        private void Awake()
+        {
+            _driver = GetComponent<CharacterControllerDriver>();
+            _characterController = GetComponent<CharacterController>();
+            _xrOrigin = GetComponent<XROrigin>();
+        }
+
+        private void Update()
+        {
+            UpdateCharacterController();
+        }
+
+        private void UpdateCharacterController()
+        {
+            if (_xrOrigin == null || _characterController == null)
+                return;
+
+            var height = Mathf.Clamp(_xrOrigin.CameraInOriginSpaceHeight, _driver.minHeight, _driver.maxHeight);
+
+            Vector3 center = _xrOrigin.CameraInOriginSpacePos;
+            center.y = height / 2f + _characterController.skinWidth;
+
+            _characterController.height = height;
+            _characterController.center = center;
+        }
+    }
+}
